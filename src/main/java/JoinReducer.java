b887a65e4e83482ffff0e45bdb8b84998aefdad8
geoHeil <e1225063@student.tuwien.ac.at>
2015-12-01 13:11:31 +0100

    initial commit

---
+++ src/main/java/JoinReducer.java
@@ -0,0 +1,23 @@
+// cc JoinReducer Reducer for join
+
+package org.myorg;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+// vv JoinReducer
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+
+  @Override
+  protected void reduce(TextPair key, Iterable<Text> values, Context context)
+      throws IOException, InterruptedException {
+
+      /* here comes the reducer code */
+      
+      
+  }
+}
+// ^^ JoinReducer
