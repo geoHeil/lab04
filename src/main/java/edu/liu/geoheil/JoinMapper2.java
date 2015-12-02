b768dbae5e730c5b15076ef8066aadceaa0a0763
geoHeil <e1225063@student.tuwien.ac.at>
2015-12-02 20:45:49 +0100

    prepare the classes

--- src/main/java/edu/liu/geoheil/JoinMapper2.java
+++ src/main/java/edu/liu/geoheil/JoinMapper2.java
@@ -4,6 +4,8 @@ package edu.liu.geoheil;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,10 +14,15 @@ public class JoinMapper2
         extends Mapper<LongWritable, Text, TextPair, Text> {
 
         /* here define the variables */
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     @Override
     protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
+
+        logger.debug("Key" + key);
+        logger.debug("Text" + value);
 
 
 /* here the code for retrieving the triples from file01 and send the prefix of the dewey_pid as key */
--- src/main/java/edu/liu/geoheil/JoinReducer.java
+++ src/main/java/edu/liu/geoheil/JoinReducer.java
@@ -4,19 +4,25 @@ package edu.liu.geoheil;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 // vv JoinReducer
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
+
+        logger.debug("Key" + key);
+        logger.debug("Text" + values);
 
       /* here comes the reducer code */
 
 
     }
 }
-// ^^ JoinReducer
