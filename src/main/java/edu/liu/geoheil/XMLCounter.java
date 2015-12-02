c5632f895e8215b57729373117a1c093f57f4279
geoHeil <e1225063@student.tuwien.ac.at>
2015-12-02 16:26:07 +0100

    part 01 finished

--- src/main/java/edu/liu/geoheil/XMLCounter.java
+++ src/main/java/edu/liu/geoheil/XMLCounter.java
@@ -5,44 +5,95 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Stack;
+
 public class XMLCounter implements ContentHandler {
 
-	/* here comes var declaration */
+    /* here comes var declaration */
+    Stack<Integer> deweyNumber;
+    // element qualified path (attr key, attr val)
+    HashMap<String, HashMap<String, String>> elemAttributes;
 
     public void startDocument() throws SAXException {
 
      /* here comes the init code */
-
+        deweyNumber = new Stack<>();
+        deweyNumber.push(1);
     }
 
     public void startElement(String uri, String localName, String qName,
                              Attributes attributes) throws SAXException {
+        /**
+         * Stuff for the elements.
+         * Saved to file manually
+         */
+        System.out.println(printStack(deweyNumber) + " " + qName);
+        deweyNumber.push(1);
 
-        System.out.println("Start Element :" + qName);
+        /**
+         * For the attributes
+         * saved manually to file
+         */
+//        for (int i = 0; i < attributes.getLength(); i++) {
+//            System.out.println(printStack(deweyNumber) + " " + attributes.getQName(i) + " " + attributes.getValue(i));
+//        }
+//        deweyNumber.push(1);
+    }
 
-        /* here comes code after the startElement is met */
-        /* for attributes too */
+    public String printStack(Stack<Integer> stack) {
+        StringBuilder result = new StringBuilder();
+        Iterator<Integer> iter = stack.iterator();
 
+        while (iter.hasNext()) {
+            result.append(iter.next());
+
+            if (iter.hasNext()) {
+                result.append(".");
+            }
+        }
+        return result.toString();
     }
 
     public void endElement(String uri, String localName,
                            String qName) throws SAXException {
-
-        System.out.println("End Element :" + qName);
-
+        deweyNumber.pop();
+        deweyNumber.set(deweyNumber.size() - 1, deweyNumber.peek() + 1);
         /* here comes code after the endElement is met */
+
     }
 
     public void characters(char ch[], int start, int length) throws SAXException {
 
+        /**
+         * not needed @Valentina
+         */
         /* here comes code after the startElement is met */
-
+//        System.out.println("start characters : " +
+//                new String(ch, start, length));
     }
 
 
     public void endDocument() throws SAXException {
+
         System.out.println("End of docuement ");
+
+        /**
+         * Written to files manually
+         */
+//        List<String> tags = new ArrayList<>();
+//        tags.add("test");
+//        writeFile(tags, "result/tags");
     }
+
+//    private void writeFile(List<String> document, String pathName) {
+//        try {
+//            Files.write(Paths.get(pathName), document);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     // Do-nothing methods we have to implement only to fulfill
     // the interface requirements:
